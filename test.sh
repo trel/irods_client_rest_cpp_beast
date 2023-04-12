f077096fe05526a23a7e994a10f66d239b142ef0
Kory Draughn <korydraughn@renci.org>
2023-04-12 15:33:40 -0400

    Updated script to use a better query.

--- test.sh
+++ test.sh
@@ -11,5 +11,5 @@ bearer_token=$(curl -X POST -H "authorization: Basic $creds" "${base_url}/auth")
 
 curl -H "authorization: Bearer $bearer_token" "${base_url}/collections?op=stat&lpath=/tempZone/home/kory" -v | jq
 curl -H "authorization: Bearer $bearer_token" "${base_url}/collections?op=list&lpath=/tempZone/home/kory" -v | jq
-curl -H "authorization: Bearer $bearer_token" "${base_url}/query?op=execute&query=select%20DATA_NAME" -v | jq
+curl -H "authorization: Bearer $bearer_token" "${base_url}/query?op=execute&query=select%20COLL_NAME,%20DATA_NAME" -v | jq
 curl -H "authorization: Bearer $bearer_token" "${base_url}/query?op=bogus" -v | jq
