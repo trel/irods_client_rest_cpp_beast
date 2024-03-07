77e9b78af0e4c148775cb6a7d8c82a1f0f2a1c7a
Martin Jaime Flores Jr <martinflores@renci.org>
2024-03-07 14:18:24 -0600

    [#155] Tests for Protected Resource Mode

--- test/test_auth_endpoint.py
+++ test/test_auth_endpoint.py
@@ -1,3 +1,4 @@
+from config import test_config
 import pytest
 import requests
 import base64
@@ -5,32 +6,41 @@ import re
 import logging
 import html
 
-# Pragmatic
-# Search for IRODS_HTTP_API_BASE_URL
-# and IRODS_CLIENT_VERSION
-# TOOD: Consider turning this into a fixture
-AUTH_URL = 'http://127.0.0.1:9000/irods-http-api/0.2.0/authenticate'
+skip_protected_resource = pytest.mark.skipif(
+    test_config['openid_connect']['mode'] == 'protected_resource', reason='Not supported in "protected_resource" mode.')
 
-@pytest.mark.parametrize("username, password, expected_result", [('bob', 'bob', requests.codes.ok),
-                                                                 ('', '', requests.codes.unauthorized),
+skip_client = pytest.mark.skipif(
+    test_config['openid_connect']['mode'] == 'client', reason='Not supported in "client" mode.')
+
+@pytest.fixture
+def irods_http_api_url_base():
+    return f'http://{test_config["host"]}:{test_config["port"]}{test_config["url_base"]}'
+
+@pytest.fixture
+def auth_url(irods_http_api_url_base):
+    return f'{irods_http_api_url_base}/authenticate'
+
+@pytest.mark.parametrize("username, password, expected_result", [pytest.param('bob', 'bob', requests.codes.ok, marks=skip_protected_resource),
+                                                                 pytest.param('', '', requests.codes.unauthorized, marks=skip_protected_resource),
                                                                  ('not', 'valid', requests.codes.bad_request),
-                                                                 ('a'*200, 'b'*200, requests.codes.unauthorized)])
-def test_oidc_resource_owner_password_credentials_login(username, password, expected_result):
+                                                                 pytest.param('a'*200, 'b'*200, requests.codes.unauthorized, marks=skip_protected_resource)])
+def test_oidc_resource_owner_password_credentials_login(username, password, expected_result, auth_url):
     to_be_encoded = f'{username}:{password}'
     encoded_user_pass = base64.b64encode(to_be_encoded.encode())
 
-    res = requests.post(AUTH_URL, headers={'Authorization': f'iRODS {encoded_user_pass.decode()}'})
+    res = requests.post(auth_url, headers={'Authorization': f'iRODS {encoded_user_pass.decode()}'})
 
     # Got a good code, assume we passed...
     assert res.status_code == expected_result
 
-def test_oidc_authorization_code_login():
+@skip_protected_resource
+def test_oidc_authorization_code_login(auth_url):
     # Session required for cookie
     s = requests.Session()
 
     # Go to login page
     # requests automatically redirects
-    res = s.get(AUTH_URL)
+    res = s.get(auth_url)
     assert res.status_code == requests.codes.ok
 
     data = res.text
@@ -51,13 +61,14 @@ def test_oidc_authorization_code_login():
     # Make sure we weren't redirected back the the auth page
     assert re.match('<!DOCTYPE html>', res.text) is None
 
-def test_oidc_authorization_code_non_irods_user():
+@skip_protected_resource
+def test_oidc_authorization_code_non_irods_user(auth_url):
     # Session required for cookie
     s = requests.Session()
 
     # Go to login page
     # requests automatically redirects
-    res = s.get(AUTH_URL)
+    res = s.get(auth_url)
     assert res.status_code == requests.codes.ok
 
     data = res.text
@@ -75,31 +86,33 @@ def test_oidc_authorization_code_non_irods_user():
     res = s.post(extracted_url, data={'username': 'non_irods_user', 'password': 'bad', 'credentialId': ''})
     assert res.status_code == requests.codes.bad_request
 
-
 @pytest.mark.parametrize("username, password, expected_result", [('rods', 'rods', requests.codes.ok),
                                                                  ('', '', requests.codes.unauthorized),
                                                                  ('not', 'valid', requests.codes.unauthorized),
                                                                  ('a'*200, 'b'*200, requests.codes.unauthorized)])
-def test_post_basic_login(username, password, expected_result):
-    res = requests.post(AUTH_URL, auth=(username, password))
+def test_post_basic_login(username, password, expected_result, auth_url):
+    res = requests.post(auth_url, auth=(username, password))
 
     # Got a good code, assume we passed...
     assert res.status_code == expected_result
 
+@pytest.mark.parametrize("state, code", [('placeholder', 'bad_code'),
+                                         ('', 'bad_code'),
+                                         ('placeholder', ''),
+                                         ('a'*200, 'b'*200)])
+def test_get_oidc_errors(state, code, auth_url):
+    res = requests.get(auth_url, params={'state': state, 'code': code})
 
-@pytest.mark.parametrize("state, code, expected_result", [('placeholder', 'bad_code', requests.codes.bad_request),
-                                                          ('', 'bad_code', requests.codes.bad_request),
-                                                          ('placeholder', '', requests.codes.bad_request),
-                                                          ('a'*200, 'b'*200, requests.codes.bad_request)])
-def test_get_oidc_errors(state, code, expected_result):
-    res = requests.get(AUTH_URL, params={'state': state, 'code': code})
-
-    assert res.status_code == expected_result
+    if test_config['openid_connect']['mode'] == 'client':
+        assert res.status_code == requests.codes.bad_request
+    else:
+        assert res.status_code == requests.codes.method_not_allowed
 
 @pytest.mark.parametrize("method", [requests.head,
+                                    pytest.param(requests.get, marks=skip_client),
                                     requests.put,
                                     requests.delete,
                                     requests.patch])
-def test_other_http_methods(method):
-    res = method(AUTH_URL)
+def test_other_http_methods(method, auth_url):
+    res = method(auth_url)
     assert res.status_code == requests.codes.method_not_allowed
