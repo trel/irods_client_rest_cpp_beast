10368de0991840a7ad7ba402de3216d49a46d774
Kory Draughn <korydraughn@renci.org>
2024-07-01 16:15:58 -0400

    [#324] Remove unnecessary logging functions.
    The variadic templates provided by log.hpp cover all use-cases.

--- core/include/irods/private/http_api/log.hpp
+++ core/include/irods/private/http_api/log.hpp
@@ -9,36 +9,6 @@
 
 namespace irods::http::log
 {
-	inline auto trace(const std::string_view _msg) -> void
-	{
-		spdlog::trace(_msg);
-	} // trace
-
-	inline auto info(const std::string_view _msg) -> void
-	{
-		spdlog::info(_msg);
-	} // trace
-
-	inline auto debug(const std::string_view _msg) -> void
-	{
-		spdlog::debug(_msg);
-	} // trace
-
-	inline auto warn(const std::string_view _msg) -> void
-	{
-		spdlog::warn(_msg);
-	} // trace
-
-	inline auto error(const std::string_view _msg) -> void
-	{
-		spdlog::error(_msg);
-	} // trace
-
-	inline auto critical(const std::string_view _msg) -> void
-	{
-		spdlog::critical(_msg);
-	} // trace
-
 	template <typename... Args>
 	constexpr auto trace(fmt::format_string<Args...> _format, Args&&... _args) -> void
 	{
