4e44d826fe82f963ecd8b044601707e54b6629af
Martin Jaime Flores Jr <martinflores@renci.org>
2024-12-13 17:38:22 +0000

    [#391] Add missing return to `set_log_level(...)`
    This prevents a past the end iterator dereference.

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -687,6 +687,7 @@ auto set_log_level(const json& _config) -> void
 
 	if (iter == std::end(_config)) {
 		spdlog::set_level(spdlog::level::info);
+		return;
 	}
 
 	const auto& lvl_string = iter->get_ref<const std::string&>();
